aa54d486808429be8294d5cfa95d4f21967ec536
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2021-10-03 08:48:31 +0200

    add the converter page and make it work

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Background } from "./background/background";
 import { ClearStyles } from "./clear_styles/clear_styles";
 import "./i18n";
 import { Navbar } from "./navbar/navbar";
+import { ConverterPage } from "./pages/converter/converter_page";
 import { BitcoinDashboardPage } from "./pages/dashboard/bitcoin_dashboard_page";
 import { BitcoinDetailsPage } from "./pages/details/bitcoin_details_page";
 import { PageWrapper } from "./pages/page_wrapper";
@@ -24,6 +25,9 @@ function App() {
               <PageWrapper>
                 <Navbar />
                 <Switch>
+                  <Route path={Routes.converter}>
+                    <ConverterPage />
+                  </Route>
                   <Route path={Routes.details}>
                     <BitcoinDetailsPage />
                   </Route>
---
+++ src/pages/converter/converter_page_style.ts
@@ -0,0 +1,50 @@
+import { css, SerializedStyles } from "@emotion/react";
+import { StyleContext } from "../../style_context/style_context";
+
+export function converterPageSuspendingStyle(
+  styleContext: StyleContext
+): SerializedStyles {
+  return css`
+    label: converter-page-suspending;
+    height: calc(100% - ${styleContext.sizes.height.navbar});
+    background-color: ${styleContext.shades.contentBackground};
+
+    .header {
+      display: flex;
+      width: 70%;
+      margin: auto;
+      align-content: center;
+      justify-content: space-around;
+      padding: 16px;
+      text-align: left;
+
+      > * {
+        flex: 1 0 0;
+        color: ${styleContext.shades.text};
+        font-size: ${styleContext.sizes.font.text};
+        text-align: center;
+      }
+
+      .name {
+        text-align: left;
+      }
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      overflow-y: auto;
+
+      .detail {
+        display: flex;
+
+        font-size: ${styleContext.sizes.font.text};
+
+        &-label {
+          flex: 0 0 400px;
+        }
+      }
+    }
+  `;
+}
